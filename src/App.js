9f54feb75dd0a2637bf3643f3697d01c2abd19a2
Lee <leewhiz119@gmail.com>
2022-11-30 17:26:26 -0500

    added demon component, downloaded react-router-dom and created path to demon.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import './App.css';
 import DemonList from './DemonList.js'
 import Demons from './demonlist.json'
 import { useState } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Demon from './Demon.js'
 
 function App() {
   const [demons, setDemons] = useState(Demons)
   console.log(demons)
   
   return (
-    <div className="App">
-        <DemonList />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <DemonList />
+          </Route>
+          <Route path="/demon">
+            <Demon />
+          </Route>
+        </Switch>        
+      </div>
+  </Router>
   );
 }
 
---
+++ src/Demon.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Demon() {
+  return (
+    <h2>Demon Name</h2>
+  )
+}
